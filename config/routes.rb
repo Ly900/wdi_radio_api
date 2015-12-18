927729b0936babb229aea11d64199aff35d4d346
Ly Nguyen <Ly900@aol.com>
2015-12-18 16:04:02 -0500

    devise token auth prefix added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :songs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: redirect('/songs')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
